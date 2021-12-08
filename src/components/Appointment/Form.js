bb8b4b1c090fd3f88516ad4191af8639ec782f8e
Xihai Luo <xihai.luo01@gmail.com>
2021-12-08 18:38:17 -0500

    add Form component and stories

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  function reset() {
+    setStudent("");
+    setInterviewer("");
+  }
+  function cancel() {
+    reset();
+    props.onCancel();
+  }
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form onSubmit={(e) => e.preventDefault()} autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(e) => {
+              setStudent(e.target.value);
+            }}
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button onClick={cancel} danger>
+            Cancel
+          </Button>
+          <Button onClick={props.onSave} confirm>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
+}
