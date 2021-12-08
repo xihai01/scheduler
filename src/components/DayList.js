2f7090081a070ee93f7ec0d99127fc6c2b965e43
Xihai Luo <xihai.luo01@gmail.com>
2021-12-08 15:03:43 -0500

    refactor interview list and day list components to use the name value and onChange

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,15 +2,15 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  const { days, day, setDay } = props;
+  const { days, value, onChange } = props;
   const dayListItems = days.map((dayItem) => {
     return (
       <DayListItem
         key={dayItem.id}
         name={dayItem.name}
         spots={dayItem.spots}
-        selected={dayItem.name === day}
-        setDay={setDay}
+        selected={dayItem.name === value}
+        setDay={onChange}
       />
     );
   });
