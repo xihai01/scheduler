947ba987bbe9ab206bdd7e8e62d791d344025475
Xihai Luo <xihai.luo01@gmail.com>
2021-12-10 19:04:26 -0500

    add custom hook useVisualMode to Appointment component to control its render states

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,21 +1,44 @@
 import React, { Fragment } from "react";
+import { useVisualMode } from "hooks/useVisualMode";
 import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview && (
+      {mode === SHOW && (
         <Show
           time={props.time}
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
       )}
-      {!props.interview && <Empty />}
+      {mode === EMPTY && (
+        <Empty
+          onAdd={() => {
+            transition(CREATE);
+          }}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={() => {
+            back();
+          }}
+        />
+      )}
     </article>
   );
 }
