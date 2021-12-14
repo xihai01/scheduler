80a4769a93476b5890639a01520335fcde4b9bff
Xihai Luo <xihai.luo01@gmail.com>
2021-12-13 20:00:29 -0500

    add spots counter to keeo track of empty appointment slots for each day

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -35,6 +35,32 @@ export function useApplicationData() {
     setState({ ...state, day });
   };
 
+  //input: app. array, state, set state fcn
+  const updateDaySpots = function (appointments) {
+    let spots = 0;
+    //get the days appointments
+    const listOfAppForDay = state.days.filter((d) => {
+      return d.name === state.day;
+    })[0].appointments;
+    //count number of spots left
+    for (const app of listOfAppForDay) {
+      if (appointments[app].interview === null) {
+        spots++;
+      }
+    }
+    //update days state with number of spots
+    let dayCpy = [];
+    for (const d of state.days) {
+      let dCpy = { ...d };
+      if (dCpy.name === state.day) {
+        dCpy.spots = spots;
+      }
+      dayCpy.push(dCpy);
+    }
+    console.log(dayCpy);
+    return dayCpy;
+  };
+
   //change the local state when an interview is booked - PUT
   const bookInterview = function (id, interview) {
     console.log(id, interview);
@@ -57,12 +83,13 @@ export function useApplicationData() {
         console.log("hi");
         if (response.status === 204) {
           console.log("hello");
-          setState({ ...state, appointments });
+          let days = updateDaySpots(appointments);
+          setState({ ...state, appointments, days });
         }
         return;
       })
       .catch((error) => {
-        console.log("errr");
+        console.log(error);
         return Promise.reject(error);
       });
   };
@@ -81,7 +108,9 @@ export function useApplicationData() {
       .delete(`/api/appointments/${id}`)
       .then((response) => {
         if (response.status === 204) {
-          setState({ ...state, appointments });
+          let days = updateDaySpots(appointments);
+          setState({ ...state, appointments, days });
+          //updateDaySpots();
         }
         return;
       })
