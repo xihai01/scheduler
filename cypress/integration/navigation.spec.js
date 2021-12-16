53d2cd56e2f2083bdba1cda19d92f617aa1aece3
Xihai Luo <xihai.luo01@gmail.com>
2021-12-16 10:06:41 -0500

    add cypress test for verifying day list items work

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,4 +2,13 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    //get list element for Tuesday and click on it
+    //verify a white background appears when Tuesday is selected
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
+  });
 });
